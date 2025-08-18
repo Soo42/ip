801a41fb0ea37ea13862cb099d56ac8195fad355
Soo42 <soojiayang@gmail.com>
2025-08-18 23:55:47 +0800

    Add exit message

--- src/main/java/Aries.java
+++ src/main/java/Aries.java
@@ -6,5 +6,7 @@ public class Aries {
         System.out.println("Hello! I'm Aries.");
         System.out.println("What can I do for you?");
         System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
