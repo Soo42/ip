c6808bb0e7968034ba8121bb7fb1f0964f523b56
Soo42 <soojiayang@gmail.com>
2025-08-18 23:53:42 +0800

    Add greeting

--- src/main/java/Aries.java
+++ src/main/java/Aries.java
@@ -1,10 +1,10 @@
 public class Aries {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "____________________________________";
+
+        System.out.println(line);
+        System.out.println("Hello! I'm Aries.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
     }
 }
