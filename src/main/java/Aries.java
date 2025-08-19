846052c2f28427522f1c072bdfffbc8d9db46055
Soo42 <soojiayang@gmail.com>
2025-08-19 12:54:30 +0800

    change to switch case

--- src/main/java/Aries.java
+++ src/main/java/Aries.java
@@ -17,29 +17,31 @@ public class Aries {
         while (true) {
             String input = scanner.nextLine();
 
-            // break the loop only when user inputs "bye"
-            if (input.equals("bye")) {
-                System.out.println(line);
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(line);
-                break;
-            } else if (input.equals("list")) {
-                // print the list of tasks
-                System.out.println(line);
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
-                }
-                System.out.println(line);
-            } else {
-                // add the input to the tasks list
-                tasks[taskCount++] = new Task(input);
+            switch (input.toLowerCase()) {
+                // exit the program
+                case "bye":
+                    System.out.println(line);
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println(line);
+                    scanner.close();
+                    return;
 
-                System.out.println(line);
-                System.out.println("added: " + input);
-                System.out.println(line);
+                // print the list of tasks
+                case "list":
+                    System.out.println(line);
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    }
+                    System.out.println(line);
+
+                // add the input to the tasks list
+                default:
+                    tasks[taskCount++] = new Task(input);
+                    System.out.println(line);
+                    System.out.println("added: " + input);
+                    System.out.println(line);
+                    break;
             }
         }
-
-        scanner.close();
     }
 }
