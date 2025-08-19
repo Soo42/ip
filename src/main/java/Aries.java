d2d0a8850502b5c60939f2872413b89f84d1e5ab
Soo42 <soojiayang@gmail.com>
2025-08-19 12:06:38 +0800

    Add user input into a list

--- src/main/java/Aries.java
+++ src/main/java/Aries.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Aries {
     public static void main(String[] args) {
         String line = "____________________________________";
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         System.out.println(line);
         System.out.println("Hello! I'm Aries.");
@@ -21,12 +23,21 @@ public class Aries {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(line);
                 break;
-            }
+            } else if (input.equals("list")) {
+                // print the list of tasks
+                System.out.println(line);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(line);
+            } else {
+                // add the input to the tasks list
+                tasks[taskCount++] = input;
 
-            // print the input back
-            System.out.println(line);
-            System.out.println(input);
-            System.out.println(line);
+                System.out.println(line);
+                System.out.println("added: " + input);
+                System.out.println(line);
+            }
         }
 
         scanner.close();
