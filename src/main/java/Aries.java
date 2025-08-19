1533ad717974a1d669e533f7aaa6857eda23a472
Soo42 <soojiayang@gmail.com>
2025-08-19 11:51:09 +0800

    Echo commands back and exit with bye

--- src/main/java/Aries.java
+++ src/main/java/Aries.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Aries {
     public static void main(String[] args) {
         String line = "____________________________________";
@@ -6,7 +8,25 @@ public class Aries {
         System.out.println("Hello! I'm Aries.");
         System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        // Scanner is inspired by ex3 in cs2030s
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            // break the loop only when user inputs "bye"
+            if (input.equals("bye")) {
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+
+            // print the input back
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+        }
     }
 }
