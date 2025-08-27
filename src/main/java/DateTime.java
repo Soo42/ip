76c1be9ba80a6a1b6d85003e8e41a2984df22e92
Soo42 <soojiayang@gmail.com>
2025-08-27 20:49:50 +0800

    Add date and time

---
+++ src/main/java/DateTime.java
@@ -0,0 +1,35 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DateTime {
+    private DateTime() {}
+
+    private static final String[] DATE_TIME_PATTERN = new String[]{
+            "yyyy-MM-dd HHmm", "yyyy-MM-dd", "dd-MM-yyyy HHmm", "dd-MM-yyyy",
+            "dd/MM/yyyy HHmm", "dd/MM/yyyy", "yyyy/MM/dd HHmm", "yyyy/MM/dd", 
+            "yyyy.MM.dd HHmm", "yyyy.MM.dd", "dd.MM.yyyy HHmm", "dd.MM.yyyy", 
+            "dd MMM yyyy HHmm", "dd MMM yyyy", "yyyy MMM dd HHmm", "yyyy MMM dd"
+            };
+
+    public static LocalDateTime parse(String dateTimeStr) throws DateTimeParseException {
+        for (String pattern : DATE_TIME_PATTERN) {
+            try {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+                if (pattern.contains("HHmm")) {
+                    return LocalDateTime.parse(dateTimeStr, formatter);
+                } else {
+                    LocalDate date = LocalDate.parse(dateTimeStr, formatter);
+                    return date.atStartOfDay();
+                }
+            } catch (DateTimeParseException e) {}
+        }
+        
+        throw new DateTimeParseException("Invalid date/time format: " + dateTimeStr, dateTimeStr, 0);
+    }
+
+    public static String format(LocalDateTime dateTime) {
+        return dateTime.format(DateTimeFormatter.ofPattern("MMM d yyyy, h:mma")).toUpperCase();
+    }
+}
