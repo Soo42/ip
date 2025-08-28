aca77844b18f6cae8faf1e2a7a87040669a9fdac
Soo42 <soojiayang@gmail.com>
2025-08-28 23:42:58 +0800

    Add Aries JavaDoc

--- src/main/java/aries/AriesException.java
+++ src/main/java/aries/AriesException.java
@@ -1,5 +1,9 @@
 package aries;
 
+/**
+ * Custom exception class for Aries application.
+ * Used to handle application-specific errors.
+ */
 public class AriesException extends Exception {
     public AriesException(String message) {
         super(message);
