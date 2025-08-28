f5b41e4f1770ae5521de25dbdda350ef25802742
Soo42 <soojiayang@gmail.com>
2025-08-28 23:38:21 +0800

    Add Ui JavaDoc

--- src/main/java/aries/ui/Ui.java
+++ src/main/java/aries/ui/Ui.java
@@ -5,10 +5,17 @@ import java.util.Scanner;
 import aries.task.Task;
 import aries.task.TaskList;
 
+/**
+ * Ui class handles all interactions with the user.
+ * It reads user input and displays messages to the user.
+ */
 public class Ui {
     private static final String line = "____________________________________";
     private final Scanner scanner = new Scanner(System.in);
 
+    /**
+     * Greets the user with a welcome message.
+     */
     public void greet() {
         System.out.println(line);
         System.out.println("Hello! I'm Aries.");
@@ -16,6 +23,9 @@ public class Ui {
         System.out.println(line);
     }
 
+    /**
+     * Bids farewell to the user and closes the scanner.
+     */
     public void exit() {
         System.out.println(line);
         System.out.println("Bye. Hope to see you again soon!");
@@ -23,20 +33,38 @@ public class Ui {
         close();
     }
 
+    /**
+     * Reads a line of input from the user.
+     *
+     * @return The line of input entered by the user.
+     */
     public String read() {
         return scanner.nextLine();
     }
 
+    /**
+     * Displays an error message to the user.
+     *
+     * @param message The error message to be displayed.
+     */
     public void showError(String message) {
         System.out.println(line);
         System.out.println(message);
         System.out.println(line);
     }
 
+    /**
+     * Closes the scanner to prevent resource leaks.
+     */
     public void close() {
         scanner.close();
     }
 
+    /**
+     * Displays the list of tasks to the user.
+     *
+     * @param tasks The TaskList containing the tasks to be displayed.
+     */
     public void showTaskList(TaskList tasks) {
         System.out.println(line);
         System.out.println("Here are the tasks in your list:");
@@ -46,6 +74,11 @@ public class Ui {
         System.out.println(line);
     }
 
+    /**
+     * Displays a message indicating that a task has been added.
+     *
+     * @param tasks The TaskList after addition.
+     */
     public void added(TaskList tasks) {
         System.out.println(line);
         System.out.println("Got it. I've added this task:");
@@ -54,6 +87,12 @@ public class Ui {
         System.out.println(line);
     }
 
+    /**
+     * Displays a message indicating that a task has been deleted.
+     *
+     * @param task  The Task that was deleted.
+     * @param tasks The TaskList after deletion.
+     */
     public void deleted(Task task, TaskList tasks) {
         System.out.println(line);
         System.out.println("Noted. I've removed this task:");
@@ -62,6 +101,12 @@ public class Ui {
         System.out.println(line);
     }
 
+    /**
+     * Displays a message indicating that a task has been marked as done or not done.
+     *
+     * @param task   The Task to be marked.
+     * @param isDone A boolean indicating whether the task is marked as done (true) or not done (false).
+     */
     public void marked(Task task, boolean isDone) {
         System.out.println(line);
         System.out.println(isDone ? "Nice! I've marked this task as done:" : "OK, I've marked this task as not done yet:");
