eaf2440614236b6dfaa7fe14afbe17100891fe1c
Soo42 <soojiayang@gmail.com>
2025-09-03 14:19:15 +0800

    Fix Storage.java style error

--- src/main/java/aries/storage/Storage.java
+++ src/main/java/aries/storage/Storage.java
@@ -5,9 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream
-
-;
+import java.io.ObjectOutputStream;
 
 import aries.task.TaskList;
 
